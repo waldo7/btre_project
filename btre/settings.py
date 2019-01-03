8a77cf2bf248349502cad80cf0fb320067b776ce
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-03 17:46:55 +0800

    Add Realtors model and run migrations

--- btre/settings.py
+++ btre/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig'
+
 ]
 
 MIDDLEWARE = [
--- listings/models.py
+++ listings/models.py
@@ -20,19 +20,20 @@ class Listing(models.Model):
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     is_published = models.BooleanField(default=True)
     list_date = models.DateField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     photo_main = models.ImageField(upload_to="photos/%Y/%m/%d/")
     photo_1 = models.ImageField(
-        upload_to="photos/%Y/%m/%d/", black=True, null=True)
+        upload_to="photos/%Y/%m/%d/", blank=True, null=True)
     photo_2 = models.ImageField(
-        upload_to="photos/%Y/%m/%d/", black=True, null=True)
+        upload_to="photos/%Y/%m/%d/", blank=True, null=True)
     photo_3 = models.ImageField(
-        upload_to="photos/%Y/%m/%d/", black=True, null=True)
+        upload_to="photos/%Y/%m/%d/", blank=True, null=True)
     photo_4 = models.ImageField(
-        upload_to="photos/%Y/%m/%d/", black=True, null=True)
+        upload_to="photos/%Y/%m/%d/", blank=True, null=True)
     photo_5 = models.ImageField(
-        upload_to="photos/%Y/%m/%d/", black=True, null=True)
+        upload_to="photos/%Y/%m/%d/", blank=True, null=True)
     photo_6 = models.ImageField(
-        upload_to="photos/%Y/%m/%d/", black=True, null=True)
+        upload_to="photos/%Y/%m/%d/", blank=True, null=True)
 
     def __str__(self):
         return self.title
