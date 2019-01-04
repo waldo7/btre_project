3d70353baad75cf19bc3c54e46575827e134c4c8
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-05 00:02:20 +0800

    Pull data from Listing Model and display in template

--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="listings"),
+    # path("", views.index, name="listings"),
+    path("", views.ListingsView.as_view(), name="listings"),
     path("<int:listing_id>", views.listing, name="listing"),
     path("search", views.search, name="search")
 
--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Listing
 
 # Create your views here.
 
+# def index(request):
+#     return render(request, "listings/listings.html", {"name": "waldo"})
 
-def index(request):
-    return render(request, "listings/listings.html")
+
+class ListingsView(generic.ListView):
+    model = Listing
+    template_name = "listings/listings.html"
+    context_object_name = "listings"
 
 
 def listing(request, listing_id):
