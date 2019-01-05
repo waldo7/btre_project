b0cb77e91a79569066c933b668445f6e02557032
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-05 18:30:56 +0800

    Home and about page dynamic content

--- realtors/admin.py
+++ realtors/admin.py
@@ -9,7 +9,7 @@ class RealtorAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "email", "hire_date")
     list_display_links = ("id", "name")
     search_fields = ("name", )
-    list_per_page = 2
+    list_per_page = 25
 
 
 admin.site.register(Realtor, RealtorAdmin)
