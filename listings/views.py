d3b3d2729ea397a97c87a9edac30cde80a5518c9
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-08 15:08:44 +0800

    Preserving form input

--- listings/views.py
+++ listings/views.py
@@ -31,9 +31,48 @@ class ListingView(generic.DetailView):
 
 
 def search(request):
+    query_list = Listing.objects.order_by("-list_date")
+
+    # keyword
+    if "keywords" in request.GET:
+        keywords = request.GET["keywords"].strip()
+        if keywords:
+            query_list = query_list.filter(
+                description__icontains=keywords)
+
+    # city
+    if "city" in request.GET:
+        city = request.GET["city"]
+        if city:
+            query_list = query_list.filter(
+                city__iexact=city)
+
+    # state
+    if "state" in request.GET:
+        state = request.GET["state"]
+        if state:
+            query_list = query_list.filter(
+                state__iexact=state)
+
+    # bedrooms
+    if "bedrooms" in request.GET:
+        bedrooms = request.GET["bedrooms"]
+        if bedrooms:
+            query_list = query_list.filter(
+                bedrooms__gte=bedrooms)
+
+    # price
+    if "price" in request.GET:
+        price = request.GET["price"]
+        if price:
+            query_list = query_list.filter(
+                price__lte=price)
+
     context = {
         "state_choices": state_choices,
         "bedroom_choices": bedroom_choices,
-        "price_choices": price_choices
+        "price_choices": price_choices,
+        "listings": query_list,
+        "values": request.GET
     }
     return render(request, "listings/search.html", context)
