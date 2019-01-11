36085e74120db93172ae13e2994bfbab88ffdfbc
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-11 20:18:03 +0800

    Avoid double enquiry

--- contacts/views.py
+++ contacts/views.py
@@ -16,6 +16,14 @@ def contact(request):
         realtor_email = request.POST["realtor_email"]
         user_id = request.POST["user_id"]
 
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.filter(
+                listing=listing, user_id=user_id)
+            if has_contacted:
+                messages.error(request, "You have already made this enquiry.")
+                return redirect('listing', pk=listing_id)
+
         contact = Contact(listing_id=listing_id, listing=listing, name=name,
                           email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
